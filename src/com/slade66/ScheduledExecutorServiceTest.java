4a2e54ead33a948f16bf5c14eac4c18c2825022e
Qingdao88 <Qingdao88@proton.me>
2025-03-30 12:34:13 +0800

    ScheduledExecutorService

---
+++ src/com/slade66/ScheduledExecutorServiceTest.java
@@ -0,0 +1,40 @@
+package com.slade66;
+
+import org.junit.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorServiceTest {
+
+    class MyThread implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+            }
+            System.out.println("=".repeat(10));
+        }
+    }
+
+    @Test
+    public void basicUsage1() throws InterruptedException {
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
+            private int i = 1;
+
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setName("MyThread-" + i++);
+                return t;
+            }
+        });
+
+        scheduledExecutorService.scheduleWithFixedDelay(new MyThread(), 0, 5, TimeUnit.SECONDS);
+
+        Thread.sleep(10 * 60 * 1000);
+    }
+
+}
