575c7ca8d491222e99b783620551c21bdb73c8f7
slade <slade66@tutamail.com>
2025-04-13 14:03:37 +0800

    多线程下载器：确定每个线程要下载的字节范围

--- src/main/java/com/slade66/MultiThreadDownloader.java
+++ src/main/java/com/slade66/MultiThreadDownloader.java
@@ -9,40 +9,73 @@ import java.net.http.HttpClient;
 import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 public class MultiThreadDownloader {
 
     private final String downloadPath;
+    private final long chunkSize = 5 * 1024 * 1024;
 
-    public ContentLengthAndAcceptRanges fetchContentLengthAndAcceptRanges() throws IOException, InterruptedException {
+    public ContentLengthAndAcceptRangesDTO fetchContentLengthAndAcceptRanges() throws IOException, InterruptedException {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(downloadPath)).method("HEAD", HttpRequest.BodyPublishers.noBody()).build();
         HttpResponse<Void> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.discarding());
         HttpHeaders responseHeaders = response.headers();
-        String contentLength = responseHeaders.firstValue("Content-Length").orElse("未知");
+        long contentLength = Long.parseLong(responseHeaders.firstValue("Content-Length").orElse("0"));
         String acceptRanges = response.headers().firstValue("Accept-Ranges").orElse("不支持");
-        return new ContentLengthAndAcceptRanges(contentLength, acceptRanges);
+        return new ContentLengthAndAcceptRangesDTO(contentLength, acceptRanges);
     }
 
     @RequiredArgsConstructor
     @Getter
-    private static class ContentLengthAndAcceptRanges {
-        private final String contentLength;
+    private static class ContentLengthAndAcceptRangesDTO {
+        private final long contentLength;
         private final String acceptRanges;
     }
 
     private void run() {
         try {
-            ContentLengthAndAcceptRanges contentLengthAndAcceptRanges = fetchContentLengthAndAcceptRanges();
-            System.out.println("文件大小（字节）：" + contentLengthAndAcceptRanges.getContentLength());
-            System.out.println("是否支持分块下载：" + contentLengthAndAcceptRanges.getAcceptRanges());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+            ContentLengthAndAcceptRangesDTO contentLengthAndAcceptRanges = fetchContentLengthAndAcceptRanges();
+            long contentLength = contentLengthAndAcceptRanges.getContentLength();
+            String acceptRanges = contentLengthAndAcceptRanges.getAcceptRanges();
+            printContentLengthAndAcceptRanges(contentLengthAndAcceptRanges);
+            Map<Integer, List<Long>> tasks = calculateByteRangeForThreads(contentLength);
+            printTasks(tasks);
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private void printContentLengthAndAcceptRanges(ContentLengthAndAcceptRangesDTO contentLengthAndAcceptRanges) {
+        System.out.println("文件大小（字节）：" + contentLengthAndAcceptRanges.getContentLength());
+        System.out.println("服务器支持的范围单位：" + contentLengthAndAcceptRanges.getAcceptRanges());
+    }
+
+    private void printTasks(Map<Integer, List<Long>> tasks) {
+        tasks.forEach((taskId, byteRange) -> {
+            System.out.println("线程 " + taskId + " 下载范围: " + byteRange.get(0) + " - " + byteRange.get(1));
+        });
+    }
+
+    private Map<Integer, List<Long>> calculateByteRangeForThreads(long contentLength) {
+        Map<Integer, List<Long>> tasks = new HashMap<>();
+        int totalThreads = (int) Math.ceil((double) contentLength / chunkSize);
+        long startByte = 0;
+        long endByte = chunkSize - 1;
+        for (int i = 0; i < totalThreads; i++) {
+            List<Long> byteRange = new ArrayList<>();
+            byteRange.add(startByte);
+            byteRange.add(endByte);
+            tasks.put(i, byteRange);
+
+            startByte = endByte + 1;
+            endByte = Math.min(startByte + chunkSize - 1, contentLength - 1);
+        }
+        return tasks;
     }
 
     public static void main(String[] args) {
